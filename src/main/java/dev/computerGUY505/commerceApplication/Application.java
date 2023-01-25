6a5a999cb0a276d1dcec8700bfcce0292efc89aa
ade <demmmyemmy@gmail.com>
2023-01-25 16:28:41 +0400

    Changing compiler plugin to Java source 19

--- src/main/java/dev/computerGUY505/commerceApplication/Application.java
+++ src/main/java/dev/computerGUY505/commerceApplication/Application.java
@@ -1,14 +1,26 @@
 package dev.computerGUY505.commerceApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		for (String greeting : greetings) {
+			System.out.println("\n\nGreeting: " + greeting);
+		}
+	}
 	
 	public Application() {
-		System.out.println ("Inside Application");
+		System.out.println ("\n\nInside Application");
 	}
 
 	public static void main (String[] args) {
-		System.out.println ("Starting Application");
-	Application app = new Application();
+		System.out.println ("\n\nStarting Application");
+		Application app = new Application();
+		app.greet();
 	}
 
 }
