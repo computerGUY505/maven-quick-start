62e68a6a981573144bcc5cae7623eec8727be02a
ade <demmmyemmy@gmail.com>
2023-01-25 14:48:05 +0400

    Adding Application java source

---
+++ src/main/java/dev/computerGUY505/commerceApplication/Application.java
@@ -0,0 +1,14 @@
+package dev.computerGUY505.commerceApplication;
+
+public class Application {
+	
+	public Application() {
+		System.out.println ("Inside Application");
+	}
+
+	public static void main (String[] args) {
+		System.out.println ("Starting Application");
+	Application app = new Application();
+	}
+
+}
